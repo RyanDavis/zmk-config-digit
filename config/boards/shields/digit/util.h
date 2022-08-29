8c55488cb7efdab161275235386adcd8f5dc3092
jamiedsmith95 <jamie.d.smith95@gmail.com>
2022-08-30 00:10:08 +0100

    fix

--- config/boards/shields/digit/util.h
+++ config/boards/shields/digit/util.h
@@ -6,7 +6,7 @@ combo_##name { \
 };
 #define COMBOL(name, keypress, mylayers, keypos) \
 combo_##name { \
-  timeout-ms = <100>; \
+  timeout-ms = <50>; \
   bindings = <keypress>; \
   layers = <mylayers>; \
   key-positions = <keypos>; \
